d0823df8bd5497670e7f9bc23b9213cfb693fd46
midis-lnstitute <bot101.brawlstars@gmail.com>
2025-06-17 00:21:41 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,10 @@ use App\Http\Controllers\AdminController;
 use App\Http\Middleware\Admin;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', [ReportController::class, 'welcome'])->name('welcome');
 Route::get('/dashboard', [ReportController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
@@ -15,14 +17,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::post('/store', [ReportController::class, 'store'])->name('reports.store');
-    Route::get('/create', [ReportController::class, 'create'])->name('reports.create');
 });
 
-Route::middleware((Admin::class))->group(function(){
-    Route::get('/admin', [AdminController::class, 'index']) -> name('admin.index');
-    Route::put('/update', [ReportController::class, 'update'])->name('reports.update');
-});
+
 
 require __DIR__.'/auth.php';
+// Route::get('/', [ReportController::class, 'welcome'])->name('welcome');
+// Route::post('/store', [ReportController::class, 'store'])->name('reports.store');
+// Route::get('/create', [ReportController::class, 'create'])->name('reports.create');
+// Route::middleware((Admin::class))->group(function(){
+//     Route::get('/admin', [AdminController::class, 'index']) -> name('admin.index');
+//     Route::put('/update', [ReportController::class, 'update'])->name('reports.update');
+// });
